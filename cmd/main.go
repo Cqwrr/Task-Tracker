1a25e287a45334236df1a58397e529e513cf5616
Cqwrr <116123523+Cqwrr@users.noreply.github.com>
2025-01-27 13:58:39 +0400

    modified:   cmd/main.go 	deleted:    storage.go 	cmd/storage.go 	cmd/todos.json

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,9 @@ package main
 
 func main() {
 	todos := Todos{}
+
 	storage := NewStorage[Todos]("todos.json")
 	storage.Load(&todos)
-	todos.add("buy milk")
-	todos.add("Купить ГАВНА")
-	todos.toggle(0)
 	todos.print()
 	storage.Save(todos)
 }
